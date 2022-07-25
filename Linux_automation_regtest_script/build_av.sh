764214651a4fd2551748dc5b733e32b67d45d719
Johan Hussin <johan.hussin@intel.com>
2022-07-25 11:41:30 +0800

    update defconfig path

--- Linux_automation_regtest_script/build_av.sh
+++ Linux_automation_regtest_script/build_av.sh
@@ -1,16 +1,20 @@
 #! /bin/sh
 
+LINUX_VER=5.16
+LINUX_BRANCH=socfpga-${LINUX_VER}
+
 mkdir -p /build/linux_regtest
 cd /build/linux_regtest
 
-echo "cloning repo linux-bringup\n"
-git clone --single-branch --branch socfpga-5.16_regression_test_defconfig https://github.com/intel-sandbox/linux-bringup
+#echo "cloning repo linux-bringup\n"
+#git clone --single-branch --branch socfpga-5.16_regression_test_defconfig https://github.com/intel-sandbox/linux-bringup
 
 echo "cloning repo linux-socfpga\n"
-git clone --single-branch --branch socfpga-5.16 https://github.com/intel-innersource/applications.fpga.soc.linux-socfpga
+git clone --single-branch --branch $LINUX_BRANCH https://github.com/intel-innersource/applications.fpga.soc.linux-socfpga
 
 echo "copying modified defconfig from linux bringup to linux-socfpga"
-cp /build/linux_regtest/linux-bringup/arch/arm/configs/socfpga_defconfig /build/linux_regtest/applications.fpga.soc.linux-socfpga/arch/arm/configs/
+#cp /build/linux_regtest/linux-bringup/arch/arm/configs/socfpga_defconfig /build/linux_regtest/applications.fpga.soc.linux-socfpga/arch/arm/configs/
+cp https://psg-png-arc.png.intel.com/p/psg/swip/etools/elinux/linux_regtest/defconfig/defconfig_32 /build/linux_regtest/applications.fpga.soc.linux-socfpga/arch/arm/configs/
 
 echo "Start compiling\n"
 cd /build/linux_regtest/applications.fpga.soc.linux-socfpga/
@@ -24,12 +28,12 @@ make socfpga_arria5_socdk.dtb
 make -j 12 modules
 
 echo "Move compiled to folder\n"
-mkdir -p /build/linux_regtest/arm/5.16/av
+mkdir -p /build/linux_regtest/arm/$LINUX_VER/av
 
-cp arch/arm/boot/zImage /build/linux_regtest/arm/5.16/av
-cp arch/arm/boot/dts/socfpga_arria5_socdk.dtb /build/linux_regtest/arm/5.16/av
-cp drivers/mtd/tests/mtd_readtest.ko /build/linux_regtest/arm/5.16/av
-cp drivers/mtd/tests/mtd_stresstest.ko /build/linux_regtest/arm/5.16/av
-cp drivers/mtd/tests/mtd_speedtest.ko /build/linux_regtest/arm/5.16/av
+cp arch/arm/boot/zImage /build/linux_regtest/arm/$LINUX_VER/av
+cp arch/arm/boot/dts/socfpga_arria5_socdk.dtb /build/linux_regtest/arm/$LINUX_VER/av
+cp drivers/mtd/tests/mtd_readtest.ko /build/linux_regtest/arm/$LINUX_VER/av
+cp drivers/mtd/tests/mtd_stresstest.ko /build/linux_regtest/arm/$LINUX_VER/av
+cp drivers/mtd/tests/mtd_speedtest.ko /build/linux_regtest/arm/$LINUX_VER/av
 
 echo ">>>>>>> $i DONE <<<<<<"
